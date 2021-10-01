8d141f9ea4672bdf5c9651308392f6924028550a
johnfreyes <jhonfer96@hotmail.com>
2021-09-30 19:55:23 -0500

    Add document type entity, add data context, add migration and add libraries

---
+++ huellitas.API/Data/DataContext.cs
@@ -0,0 +1,20 @@
+﻿using huellitas.API.Data.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace huellitas.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<DocumentType> DocumentTypes { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<DocumentType>().HasIndex(x => x.Description).IsUnique();
+        }
+    }
+}
---
+++ huellitas.API/Data/Entities/DocumentType.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace huellitas.API.Data.Entities
+{
+    public class DocumentType
+    {
+        public int Id { get; set; }
+
+        [Display(Name = "Tipo de documento")]
+        [MaxLength(30, ErrorMessage = "El campo {0} no puede tener más de {1} carácteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string Description { get; set; }
+    }
+}
