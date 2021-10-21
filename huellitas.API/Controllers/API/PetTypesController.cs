e8a9e08f697a334c1fe42bbefc0bdc2eaa6589ba
dianyss2011 <89366318+dianyss2011@users.noreply.github.com>
2021-10-21 01:14:43 -0500

    api petytype

---
+++ huellitas.API/Controllers/API/PetTypesController.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using huellitas.API.Data;
+using huellitas.API.Data.Entities;
+
+namespace huellitas.API.Controllers.API
+{
+    [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [Route("api/[controller]")]
+    public class PetTypesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public PetTypesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<PetType>>> GetPetTypes()
+        {
+            return await _context.PetTypes.OrderBy(x => x.Description).ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<PetType>> GetPetType(int id)
+        {
+            PetType petType = await _context.PetTypes.FindAsync(id);
+
+            if (petType == null)
+            {
+                return NotFound();
+            }
+
+            return petType;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutPetType(int id, PetType petType)
+        {
+            if (id != petType.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(petType).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+
+                return NoContent();
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                {
+                    return BadRequest("El tipo de mascota ya existe.");
+                }
+                else
+                {
+                    return BadRequest(dbUpdateException.InnerException.Message);
+                }
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<PetType>> PostPetType(PetType petType)
+        {
+            _context.PetTypes.Add(petType);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+
+                return CreatedAtAction("GetPetType", new { id = petType.Id }, petType);
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                {
+                    return BadRequest("El tipo de mascota ya existe.");
+                }
+                else
+                {
+                    return BadRequest(dbUpdateException.InnerException.Message);
+                }
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeletePetType(int id)
+        {
+            PetType petType = await _context.PetTypes.FindAsync(id);
+
+            if (petType == null)
+            {
+                return NotFound();
+            }
+
+            _context.PetTypes.Remove(petType);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
