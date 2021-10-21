f2b57c9d0977f6949b3283bdd2d9f3fc095d2e14
dianyss2011 <89366318+dianyss2011@users.noreply.github.com>
2021-10-21 01:58:40 -0500

    api services

---
+++ huellitas.API/Controllers/API/ServicesController.cs
@@ -0,0 +1,122 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using huellitas.API.Data;
+using huellitas.API.Data.Entities;
+
+namespace huellitas.API.Controllers.API
+{
+    [ApiController]
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [Route("api/[controller]")]
+    public class ServicesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public ServicesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Service>>> GetServices()
+        {
+            return await _context.Services.OrderBy(x => x.Description).ToListAsync();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Service>> GetService(int id)
+        {
+            Service service = await _context.Services.FindAsync(id);
+
+            if (service == null)
+            {
+                return NotFound();
+            }
+
+            return service;
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutService(int id, Service service)
+        {
+            if (id != service.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(service).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+
+                return NoContent();
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                {
+                    return BadRequest("El servicio ya existe.");
+                }
+                else
+                {
+                    return BadRequest(dbUpdateException.InnerException.Message);
+                }
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Service>> PostService(Service service)
+        {
+            _context.Services.Add(service);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+
+                return CreatedAtAction("GetService", new { id = service.Id }, service);
+            }
+            catch (DbUpdateException dbUpdateException)
+            {
+                if (dbUpdateException.InnerException.Message.Contains("duplicate"))
+                {
+                    return BadRequest("El servicio ya existe.");
+                }
+                else
+                {
+                    return BadRequest(dbUpdateException.InnerException.Message);
+                }
+            }
+            catch (Exception exception)
+            {
+                return BadRequest(exception.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteService(int id)
+        {
+            Service service = await _context.Services.FindAsync(id);
+
+            if (service == null)
+            {
+                return NotFound();
+            }
+
+            _context.Services.Remove(service);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
