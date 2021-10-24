30efb65341e7e1505eb72826966ecd278e14a7e9
johnfreyes <jhonfer96@hotmail.com>
2021-10-24 15:24:47 -0500

    fixed bug document type

--- huellitas.API/Helpers/CombosHelper.cs
+++ huellitas.API/Helpers/CombosHelper.cs
@@ -1,10 +1,20 @@
-﻿using Microsoft.AspNetCore.Mvc.Rendering;
+﻿using huellitas.API.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace huellitas.API.Helpers
 {
+
     public class CombosHelper : ICombosHelper
     {
+        private readonly DataContext _context;
+
+        public CombosHelper(DataContext context)
+        {
+            _context = context;
+        }
+
         public IEnumerable<SelectListItem> GetComboDocumentTypes()
         {
             List<SelectListItem> list = _context.DocumentTypes.Select(x => new SelectListItem
