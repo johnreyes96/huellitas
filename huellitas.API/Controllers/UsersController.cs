e4a8e8b7ba70350dd30131c3407354e1018d7ba6
johnfreyes <jhonfer96@hotmail.com>
2021-10-24 14:38:39 -0500

    crud users

---
+++ huellitas.API/Controllers/UsersController.cs
@@ -0,0 +1,141 @@
+﻿using huellitas.API.Data;
+using huellitas.API.Data.Entities;
+using huellitas.API.Helpers;
+using huellitas.API.Models;
+using huellitas.Common.Enums;
+using huellitas.Common.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace huellitas.API.Controllers
+{
+    [Authorize(Roles = "Admin")]
+    public class UsersController : Controller
+    {
+        private readonly DataContext _context;
+        private readonly IUserHelper _userHelper;
+        private readonly ICombosHelper _combosHelper;
+        private readonly IConverterHelper _converterHelper;
+        private readonly IBlobHelper _blobHelper;
+        private readonly IMailHelper _mailHelper;
+
+        public UsersController(DataContext context, IUserHelper userHelper, ICombosHelper combosHelper,
+            IConverterHelper converterHelper, IBlobHelper blobHelper, IMailHelper mailHelper)
+        {
+            _context = context;
+            _userHelper = userHelper;
+            _combosHelper = combosHelper;
+            _converterHelper = converterHelper;
+            _blobHelper = blobHelper;
+            _mailHelper = mailHelper;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            return View(await _context.Users
+                .Include(x => x.DocumentType)
+                .Where(x => x.UserType == UserType.User)
+                .ToListAsync());
+        }
+
+        public IActionResult Create()
+        {
+            UserViewModel model = new()
+            {
+                DocumentTypes = _combosHelper.GetComboDocumentTypes()
+            };
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(UserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Guid imageId = Guid.Empty;
+                if (model.ImageFile != null)
+                {
+                    imageId = await _blobHelper.UploadBlobAsync(model.ImageFile, "users");
+                }
+
+                User user = await _converterHelper.ToUserAsync(model, imageId, true);
+                user.UserType = UserType.User;
+                await _userHelper.AddUserAsync(user, "123456");
+                await _userHelper.AddUserToRoleAsync(user, user.UserType.ToString());
+                string myToken = await _userHelper.GenerateEmailConfirmationTokenAsync(user);
+                string tokenLink = Url.Action("ConfirmEmail", "Account", new
+                {
+                    userid = user.Id,
+                    token = myToken
+                }, protocol: HttpContext.Request.Scheme);
+
+                Response response = _mailHelper.SendMail(model.Email, "Huellitas - Confirmación de cuenta", $"<h1>Huellitas - Confirmación de cuenta</h1>" +
+                    $"Para habilitar el usuario, " +
+                    $"por favor hacer clic en el siguiente enlace: </br></br><a href = \"{tokenLink}\">Confirmar Email</a>");
+                return RedirectToAction(nameof(Index));
+            }
+            model.DocumentTypes = _combosHelper.GetComboDocumentTypes();
+            return View(model);
+        }
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return NotFound();
+            }
+
+            User user = await _userHelper.GetUserAsync(Guid.Parse(id));
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            UserViewModel model = _converterHelper.ToUserViewModel(user);
+            return View(model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(UserViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                Guid imageId = model.ImageId;
+                if (model.ImageFile != null)
+                {
+                    imageId = await _blobHelper.UploadBlobAsync(model.ImageFile, "users");
+                }
+
+                User user = await _converterHelper.ToUserAsync(model, imageId, false);
+                await _userHelper.UpdateUserAsync(user);
+                return RedirectToAction(nameof(Index));
+            }
+            model.DocumentTypes = _combosHelper.GetComboDocumentTypes();
+            return View(model);
+        }
+
+        public async Task<IActionResult> Delete(string id)
+        {
+            if (string.IsNullOrEmpty(id))
+            {
+                return NotFound();
+            }
+
+            User user = await _userHelper.GetUserAsync(Guid.Parse(id));
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            await _blobHelper.DeleteBlobAsync(user.ImageId, "users");
+            await _userHelper.DeleteUserAsync(user);
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
