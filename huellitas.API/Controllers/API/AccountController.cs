1df5cdfd663b2203e2b0a490711682a8f690aeee
dianyss2011 <89366318+dianyss2011@users.noreply.github.com>
2021-11-22 21:46:28 -0500

    change user password

--- huellitas.API/Controllers/API/AccountController.cs
+++ huellitas.API/Controllers/API/AccountController.cs
@@ -1,11 +1,15 @@
 ﻿using huellitas.API.Data.Entities;
 using huellitas.API.Helpers;
 using huellitas.API.Models;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.IdentityModel.Tokens;
 using System;
 using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
 using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
@@ -61,6 +65,36 @@ namespace huellitas.API.Controllers.API
                 }
             }
             return BadRequest();
+        }
+
+        [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+        [HttpPost]
+        [Route("ChangePassword")]
+        public async Task<IActionResult> ChangePassword(ChangePasswordViewModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                string email = User.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier).Value;
+                User user = await _userHelper.GetUserAsync(email);
+                if (user != null)
+                {
+                    IdentityResult result = await _userHelper.ChangePasswordAsync(user, model.OldPassword, model.NewPassword);
+                    if (result.Succeeded)
+                    {
+                        return NoContent();
+                    }
+                    else
+                    {
+                        return BadRequest(result.Errors.FirstOrDefault().Description);
+                    }
+                }
+                else
+                {
+                    return BadRequest("Usuario no encontrado.");
+                }
+            }
+
+            return BadRequest(ModelState);
         }
     }
 }
