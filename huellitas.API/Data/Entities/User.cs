a6b90ba9bcdd33ab6d946e431bcb62bb7311641b
dianyss2011 <89366318+dianyss2011@users.noreply.github.com>
2021-12-02 18:54:09 -0500

    add red social

--- huellitas.API/Data/Entities/User.cs
+++ huellitas.API/Data/Entities/User.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Identity;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
 namespace huellitas.API.Data.Entities
@@ -27,6 +28,12 @@ namespace huellitas.API.Data.Entities
         [Required(ErrorMessage = "El campo {0} es obligatorio.")]
         public string Document { get; set; }
 
+        [DefaultValue("57")]
+        [Display(Name = "Codigo País")]
+        [MaxLength(5, ErrorMessage = "El campo {0} no puede tener más de {1} carácteres.")]
+        [Required(ErrorMessage = "El campo {0} es obligatorio.")]
+        public string CountryCode { get; set; }
+
         [Display(Name = "Dirección")]
         [MaxLength(100, ErrorMessage = "El campo {0} no puede tener más de {1} carácteres.")]
         public string Address { get; set; }
@@ -35,9 +42,19 @@ namespace huellitas.API.Data.Entities
         public Guid ImageId { get; set; }
 
         [Display(Name = "Foto")]
-        public string ImageFullPath => ImageId == Guid.Empty
-            ? $"https://apihuellitas.azurewebsites.net/images/no_image.png"
-            : $"https://huellitasapi.blob.core.windows.net/users/{ImageId}";
+        public string ImageFullPath => LoginType == LoginType.Email
+            ? ImageId == Guid.Empty
+                ? $"https://apihuellitas.azurewebsites.net/images/no_image.png"
+                : $"https://huellitasapi.blob.core.windows.net/users/{ImageId}"
+            : string.IsNullOrEmpty(SocialImageURL)
+                ? $"https://apihuellitas.azurewebsites.net/images/no_image.png"
+                : SocialImageURL;
+
+        [Display(Name = "Foto")]
+        public string SocialImageURL { get; set; }
+
+        [Display(Name = "Tipo de login")]
+        public LoginType LoginType { get; set; }
 
         [Display(Name = "Tipo de usuario")]
         public UserType UserType { get; set; }
